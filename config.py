96cf6b05e1b40cd0f9609cb1a90dcab6d51d8b87
Jonas Liekens <jonas.liekens@brickbit.be>
2017-08-25 16:11:52 +0200

    Removed Paper theme & removed sensitive data from config.py................ -_-'

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Database
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://kwb:kwbgrasheide1234@localhost:32768/kwb'
+SQLALCHEMY_DATABASE_URI = 'database_url'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
@@ -12,21 +12,21 @@ MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 465
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
-MAIL_USERNAME = 'soulscammer'
-MAIL_PASSWORD = '!SEizCV:WUw%Hl4'
-MAIL_FROM = 'no-reply@brickbit.be'
+MAIL_USERNAME = 'username'
+MAIL_PASSWORD = 'pwd'
+MAIL_FROM = 'your_email'
 
 SECURITY_PASSWORD_HASH = 'pbkdf2_sha512'
-SECURITY_PASSWORD_SALT = '5PNHiLXoM7oOQzOdTJO9F2KOj4WwEQrVeJv36aKzJnNfnDdTTl'
-SECURITY_EMAIL_SENDER = 'no-reply@brickbit.be'
+SECURITY_PASSWORD_SALT = 'some_s3lT'
+SECURITY_EMAIL_SENDER = 'your_email'
 SECURITY_LOGIN_USER_TEMPLATE = 'login.html'
 
 # Administrator list
-ADMINS = ['jonas.liekens@brickbit.be']
+ADMINS = ['admin_email']
 
 # Security
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'a49fb3a8-795c-11e7-b5a5-be2e44b06b34'
+SECRET_KEY = 'sup3r_s3cr3t_k3y'
 
 # Pagination
 POSTS_PER_PAGE=10
